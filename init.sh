d13f1166ce61ef60075cba32baed09d94657dbcf
Jacek Bogdanski <jacekbogd@gmail.com>
2018-10-11 00:54:10 +0200

    Added tmux conf with colors support.

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 ln -sfn $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sfn $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sfn $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sfn $HOME/dotfiles/.tern-config $HOME/.tern-config
 ln -sfn $HOME/dotfiles/.termite $HOME/.config/termite/config
