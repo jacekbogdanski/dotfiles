cec27882da87fb08588e1caf69ddc64108988dd4
Jacek Bogdanski <jacekbogd@gmail.com>
2018-10-10 22:43:51 +0200

    Added termite config.

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@
 ln -sfn $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sfn $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sfn $HOME/dotfiles/.tern-config $HOME/.tern-config
+ln -sfn $HOME/dotfiles/.termite $HOME/.config/termite/config
