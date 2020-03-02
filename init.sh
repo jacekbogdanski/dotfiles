7b07b7cdb417b1b2923fda14293677a55df3a814
Jacek Bogdanski <jacekbogd@gmail.com>
2020-03-02 22:45:24 +0100

    Dropped git workaround for tags.

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ ln -sfn $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sfn $HOME/dotfiles/.alacritty.yml $HOME/.alacritty.yml
 ln -sfn $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -sfn $HOME/dotfiles/.ctags $HOME/.ctags
 
 # Initialize custom git hooks.
 git config --global init.templatedir $HOME/dotfiles/git_template/hooks
