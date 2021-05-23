4dbe77527f9d0ca8ffbdf87532f4804f4dae0c4c
Jacek Bogdanski <jacek.bogdanski@icloud.com>
2021-05-23 20:01:16 +0200

    Renaming, tree not opened by default.

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -3,7 +3,6 @@ local utils = require("utils")
 utils.set_options(vim.g, {
 	nvim_tree_lsp_diagnostics = 1,
 	nvim_tree_gitignore = 1,
-	nvim_tree_auto_open = 1,
 	nvim_tree_auto_close = 1,
 	nvim_tree_hide_dotfiles = 1,
 	nvim_tree_indent_markers = 1,
