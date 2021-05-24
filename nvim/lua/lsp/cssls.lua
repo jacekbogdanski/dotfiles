5a1525df174e1ddddf7c3aaa347210b48139ffcc
Jacek Bogdanski <jacek.bogdanski@icloud.com>
2021-05-24 17:11:42 +0200

    Html and css lsp.

---
+++ nvim/lua/lsp/cssls.lua
@@ -0,0 +1,8 @@
+-- npm install -g vscode-css-languageserver-bin
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require("lspconfig").cssls.setup {
+  capabilities = capabilities
+}
