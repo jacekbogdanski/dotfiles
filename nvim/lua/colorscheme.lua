ac7e64cb0d18ec81d136d91ebfb958ebc2a1425c
Jacek Bogdanski <jacek.bogdanski@icloud.com>
2021-05-25 19:44:55 +0200

    Colorscheme improvements.

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -5,5 +5,8 @@ utils.set_options(vim.g, {
 	oceanic_next_terminal_italic = 1
 })
 
-vim.cmd('let g:nvcode_termcolors=256')
+vim.cmd("let g:nvcode_termcolors=256")
 vim.cmd("colorscheme OceanicNext")
+
+-- Remove ~ at the end of file
+vim.cmd("hi! EndOfBuffer ctermbg=bg ctermfg=bg guibg=bg guifg=bg")
