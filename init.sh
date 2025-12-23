eb62d581ad73cb1636427518897c6153d1f07844
Jacek Bogdanski <jacek.bogdanski@icloud.com>
2025-12-23 10:26:29 +0100

    Cleanup.

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ln -sfn $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sfn $HOME/dotfiles/.alacritty.yml $HOME/.alacritty.yml
-ln -sfn $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -sfn $HOME/dotfiles/.ctags $HOME/.ctags
